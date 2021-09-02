f9f7ae45fd5daca7300439e024bf8cc9960fdc3e
nguyen-william93 <=>
2021-09-01 20:55:37 -0500

    update javascript to switch between screen, get user answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,86 @@
 //score logic will be in the score.js
 var startButton = document.querySelector("#start-quiz");
 var timeLeft = 100;
+var index = 0;
+var userData = {
+    name: "",
+    score: 0,
+}
 var questionBankObj = [
     {
         title: "what is your name?",
-        choices: ["nana", "kim anh", "bee", "All of these above"],
-        answer: "All of these above",
+        choices: ["1. nana", "2. kim anh", "3. bee", "4. All of these above"],
+        answer: "4. All of these above",
     },
 ];
 
-var startquiz = function(){
-    console.log("quiz start");
+//obtaingin the question and display it as within the h2 tag on the question screen.
+var getQuestion = function(index){
+    var question = document.querySelector("#question-screen h2");
+    question.textContent = questionBankObj[index].title;
+};
+
+//displaying answer choices and obtain the user answer
+var getChoices = function(index){
+    var choiceIndex = questionBankObj[index].choices;
+    var answerList = document.querySelector("#answer-list")
+
+    for (var i = 0; i < choiceIndex.length; i++){
+        var choiceNode = document.createElement("li");
+
+        choiceNode.setAttribute ("id", "submit");
+        choiceNode.setAttribute ("class", "answer-choice");
+        choiceNode.textContent = choiceIndex[i];
+
+        answerList.appendChild(choiceNode);
+    }
+
+    //obtaingin the user answer answer base on what user clicked
+    var getUserChoices = document.querySelectorAll("#submit");
+    for (var i = 0; i < getUserChoices.length; i++){
+        getUserChoices[i].addEventListener("click", checkAnswer);
+    };
+};
+
+//checking the user answer and update the timer
+var checkAnswer = function(event){
+    var userAnswer = event.target.textContent;
+    var correctAnswer = questionBankObj[index].answer;
+
+    if (userAnswer !== correctAnswer){
+        timeLeft -= 20;
+        console.log(timeLeft);
+    }  
+};
+
+//displaying the question section, displaying the score section
+var displayQuestion = function(){
+    var startScreen = document.querySelector("#start-screen");
+    var questionScreen = document.querySelector("#question-screen");
+
+    startScreen.setAttribute("class", "hide");
+    questionScreen.setAttribute("class", "");
 }
 
-startButton.addEventListener("click", startquiz);
+var displayingScore = function(){
+    var questionScreen = document.querySelector("#question-screen");
+    var scoreScreen = document.querySelector("#score-screen");
+
+    questionScreen.setAttribute("class", "hide");
+    scoreScreen.setAttribute("class", "");
+}
+
+//running the timer
+var startTimer = function(){
+};
+
+//running the quiz
+var startQuiz = function(){
+    displayQuestion();
+    getQuestion(index);
+    getChoices(index);
+};
+
+
+
+startButton.addEventListener("click", startQuiz);
