4ecb01ee7153ca1773fddf0002986d0f442d9fa4
nguyen-william93 <=>
2021-09-03 14:28:31 -0500

    add saved score to save user highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,127 @@
-// event listen to start the quiz
-// when the quiz started decreasing the timer
-// displying the question -- if the timer is not zero or there is still question left
-// event listener to whatever answer the user chooses
-// check that answer against the correct answer
-// right --> move on, false --> subtract 10 second then move on
-// check the timer again to make sure it's not zero, if it's zero --> display a message
-//if the timer is not zero and the user got through the quiz --> display the score and ask the user to input a name for the high score
-// after getting the score then added it to the high score list and store it locally.
-//score logic will be in the score.js
-var startButton = document.querySelector("#start-quiz");
-var timeLeft = 100;
+//declaring global variable and question bank obj
+var startButoon =  document.querySelector("#start-quiz");
+var questionScreen = document.querySelector("#question-screen");
+var multipleChoice = document.querySelector("#answer-list");
+var time = document.querySelector("#time");
+var feedBack = document.querySelector("#feed-back");
+var submitEl = document.querySelector("#submit");
+var timeLeft = 20;
 var index = 0;
-var userData = {
-    name: "",
-    score: 0,
-}
+var timerFunction;
 var questionBankObj = [
     {
-        title: "what is your name?",
-        choices: ["1. nana", "2. kim anh", "3. bee", "4. All of these above"],
-        answer: "4. All of these above",
+        question: "hi what is your nane?",
+        choices: ["a", "b", "c", "d"],
+        correctAnswer: "a"
     },
+    {
+        question: "How are you doing?",
+        choices: ["a","b","c","d"],
+        correctAnswer: "c"
+    }  
 ];
 
-//obtaingin the question and display it as within the h2 tag on the question screen.
-var getQuestion = function(index){
-    var question = document.querySelector("#question-screen h2");
-    question.textContent = questionBankObj[index].title;
+
+var quizEnd = function(){
+    clearInterval(timerFunction);
+
+    var scoreScreen = document.querySelector("#score-screen");
+    scoreScreen.removeAttribute("class");
+
+    var finalScore = document.querySelector("#final-score");
+    finalScore.textContent = timeLeft;
+
+    questionScreen.setAttribute("class", "hide");
+
+}
+
+var checkAnswer = function(){
+    if (this.value !== questionBankObj[index].correctAnswer){
+        timeLeft -= 10;
+        if (timeLeft < 0){
+            timeLeft = 0;
+        }
+        time.textContent = timeLeft;
+        feedBack.textContent = "Wrong!";
+        feedBack.setAttribute("class", "feed-back-wrong");
+    } else {
+        feedBack.textContent = "Correct!";
+        feedBack.setAttribute("class", "feed-back-correct");
+    };
+
+    feedBack.setAttribute("class", "feed-back");
+    setTimeout(function(){
+        feedBack.setAttribute("class", "feed-back hide");
+    }, 1000);
+
+    index += 1;
+    if(index === questionBankObj.length){
+        quizEnd();
+    } else {
+        getQuestion();
+    }
 };
 
-//displaying answer choices and obtain the user answer
-var getChoices = function(index){
-    var choiceIndex = questionBankObj[index].choices;
-    var answerList = document.querySelector("#answer-list")
+var getQuestion=  function(){
+    var currentQuestion = questionBankObj[index];
 
-    for (var i = 0; i < choiceIndex.length; i++){
+    var questionTitle = document.querySelector("#question-screen h2");
+    questionTitle.textContent = currentQuestion.question;
+
+    multipleChoice.innerHTML = "";
+
+    for (var i = 0; i < 4; i ++){
         var choiceNode = document.createElement("li");
+        var buttonEl = document.createElement("button")
+        buttonEl.setAttribute("class", "choice");
+        buttonEl.setAttribute("value", currentQuestion.choices[i]);
 
-        choiceNode.setAttribute ("id", "submit");
-        choiceNode.setAttribute ("class", "answer-choice");
-        choiceNode.textContent = choiceIndex[i];
+        buttonEl.textContent = i + 1 + ". " + currentQuestion.choices[i];
 
-        answerList.appendChild(choiceNode);
-    }
+        buttonEl.addEventListener ("click", checkAnswer);
+        
+        choiceNode.appendChild(buttonEl);
 
-    //obtaingin the user answer answer base on what user clicked
-    var getUserChoices = document.querySelectorAll("#submit");
-    for (var i = 0; i < getUserChoices.length; i++){
-        getUserChoices[i].addEventListener("click", checkAnswer);
+        multipleChoice.appendChild(choiceNode);
+
     };
 };
 
-//checking the user answer and update the timer
-var checkAnswer = function(event){
-    var userAnswer = event.target.textContent;
-    var correctAnswer = questionBankObj[index].answer;
 
-    if (userAnswer !== correctAnswer){
-        timeLeft -= 20;
-        console.log(timeLeft);
-    }  
-};
-
-//displaying the question section, displaying the score section
-var displayQuestion = function(){
-    var startScreen = document.querySelector("#start-screen");
-    var questionScreen = document.querySelector("#question-screen");
-
-    startScreen.setAttribute("class", "hide");
-    questionScreen.setAttribute("class", "");
-}
-
-var displayingScore = function(){
-    var questionScreen = document.querySelector("#question-screen");
-    var scoreScreen = document.querySelector("#score-screen");
-
-    questionScreen.setAttribute("class", "hide");
-    scoreScreen.setAttribute("class", "");
-}
-
-//running the timer
-var startTimer = function(){
-};
-
-//running the quiz
 var startQuiz = function(){
-    displayQuestion();
-    getQuestion(index);
-    getChoices(index);
+    var startScreen = document.querySelector("#start-screen");
+    startScreen.setAttribute("class", "hide");
+    
+    questionScreen.setAttribute("class", "");
+    getQuestion();
+    time.textContent = timeLeft;
+    timerFunction = setInterval(function(){
+        timeLeft -= 1;
+        time.textContent = timeLeft;
+        if (timeLeft <= 0){
+            timeLeft = 0;
+            quizEnd();
+            saveScore();
+        }
+    }, 1000);
+
 };
 
+var saveScore = function(){
+    var userName = document.querySelector("#name").value.trim();
+    if (userName !== ""){
+        var highscore = JSON.parse(localStorage.getItem("highscore")) || [];
+        var userObj = {
+            name: userName,
+            score: timeLeft
+        }
+        
+        highscore.push(userObj);
+        window.localStorage.setItem("highscore", JSON.stringify(highscore));
 
+        window.location.href = "score.html";
+    }
+}
+submitEl.addEventListener("click", saveScore);
+startButoon.addEventListener("click", startQuiz);
 
-startButton.addEventListener("click", startQuiz);
